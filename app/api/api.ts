834f33112f51013ca140f72306632d3b43d2b9e8
Uffaser <gerasimenkom405@gmail.com>
2026-05-10 01:51:06 +0300

    fix: update baseURL to correct API endpoint

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://notehub-api.goit.study',
+  baseURL: 'https://zero5-mail-and-img-1sy5.onrender.com',
   withCredentials: true,
 });
